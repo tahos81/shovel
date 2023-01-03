a9a7e9176987b0850b44682f8292ef5a5254a07b
zetsuboii <hmzkrb@gmail.com>
2023-01-04 00:38:16 +0300

    get_transfers module

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod starknet_demo;
 
 #[tokio::main]
 async fn main() {
-    starknet_demo::get_events::run().await;
+    starknet_demo::get_transfers::run().await;
 }
--- src/starknet_demo/mod.rs
+++ src/starknet_demo/mod.rs
@@ -1,1 +1,2 @@
 pub mod get_events;
+pub mod get_transfers;
