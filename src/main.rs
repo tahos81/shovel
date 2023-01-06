9d83268eb35ea67d93dbaa2a9428ff3e7e4e000b
tahos81 <tahirozpala@gmail.com>
2023-01-06 13:04:24 +0300

    mongodb init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 mod starknet_demo;
 
+use ::dotenv::dotenv;
+use mongodb::{options::ClientOptions, *};
+use std::env;
+
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let client_url_with_options =
+        env::var("CLIENT_URL_WITH_OPTIONS").expect("configure your .env file");
+    let client_options = ClientOptions::parse(client_url_with_options).await.unwrap();
+
+    let client = Client::with_options(client_options).unwrap();
+    let erc721_tokens = client.database("erc721_tokens");
     starknet_demo::jsonrpc_get_events::run().await;
-    //starknet_demo::get_transfers::run().await;
 }
