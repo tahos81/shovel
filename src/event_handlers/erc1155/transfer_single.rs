417d12497ea3ddb6d1420d6e0a869c64b59544da
zetsuboii <hmzkrb@gmail.com>
2023-01-27 06:19:22 +0300

    Migrate Erc1155 TransferSingle event

--- src/event_handlers/erc1155/transfer_single.rs
+++ src/event_handlers/erc1155/transfer_single.rs
@@ -1,11 +1,17 @@
 use crate::{
-    common::cairo_types::CairoUint256,
+    common::types::CairoUint256,
     db::{
-        collection::{ContractMetadataCollectionInterface, Erc1155CollectionInterface},
-        document::{ContractMetadata, Erc1155Balance},
+        collection::{
+            ContractMetadataCollectionInterface, Erc1155CollectionInterface,
+            Erc1155MetadataCollectionInterface,
+        },
+        document::{ContractMetadata, Erc1155Balance, Erc1155Metadata},
     },
-    event_handlers::context::EventContext,
-    rpc::metadata::contract,
+    event_handlers::context::Event,
+    rpc::metadata::{
+        contract,
+        token::{self, get_token_metadata},
+    },
 };
 use color_eyre::eyre::Result;
 use mongodb::{Collection, Database};
@@ -19,16 +25,16 @@ pub async fn run(
     rpc: &JsonRpcClient<HttpTransport>,
     db: &Database,
 ) -> Result<()> {
-    let _operator = event.data[0];
     let sender = event.data[1];
     let recipient = event.data[2];
     let token_id = CairoUint256::new(event.data[3], event.data[4]);
     let amount = CairoUint256::new(event.data[5], event.data[6]);
     let erc1155_collection = db.collection::<Erc1155Balance>("erc1155_tokens");
+    let erc1155_metadata_collection = db.collection::<Erc1155Metadata>("erc1155_metadata");
     let contract_metadata_collection = db.collection::<ContractMetadata>("contract_metadata");
 
     // Create the event context
-    let context = EventContext::new(event, rpc, db);
+    let event_context = Event::new(event, rpc, db);
 
     handle_transfer(
         sender,
@@ -36,8 +42,9 @@ pub async fn run(
         token_id,
         amount,
         &erc1155_collection,
+        &erc1155_metadata_collection,
         &contract_metadata_collection,
-        &context,
+        &event_context,
     )
     .await
 }
@@ -48,40 +55,55 @@ pub async fn handle_transfer(
     token_id: CairoUint256,
     amount: CairoUint256,
     erc1155_collection: &Collection<Erc1155Balance>,
+    erc1155_metadata_collection: &Collection<Erc1155Metadata>,
     contract_metadata_collection: &Collection<ContractMetadata>,
-    context: &EventContext<'_, '_>,
+    event_context: &Event<'_, '_>,
 ) -> Result<()> {
-    let contract_address = context.contract_address();
-    let block_id = context.block_id();
+    let contract_address = event_context.contract_address();
+    let block_id = event_context.block_id();
 
     // Update from balance
-    if sender != FieldElement::ZERO {
+    if sender == FieldElement::ZERO {
+        // Check if contract metadata exists
+        let contract_metadata_exists =
+            contract_metadata_collection.contract_metadata_exists(contract_address).await?;
+
+        if !contract_metadata_exists {
+            let name = contract::get_name(contract_address, &block_id, event_context.rpc).await;
+            let symbol = contract::get_symbol(contract_address, &block_id, event_context.rpc).await;
+            let contract_metadata = ContractMetadata::new(contract_address, name, symbol);
+            contract_metadata_collection.insert_contract_metadata(contract_metadata).await?;
+        }
+
+        // Check if token metadata exists
+        let token_metadata_exists =
+            erc1155_metadata_collection.erc1155_metadata_exists(contract_address, token_id).await?;
+
+        if !token_metadata_exists {
+            let token_uri =
+                token::get_erc1155_uri(contract_address, &block_id, event_context.rpc, token_id)
+                    .await;
+            let metadata = get_token_metadata(&token_uri).await?;
+            let erc1155_metadata =
+                Erc1155Metadata::new(contract_address, token_id, token_uri, metadata);
+
+            erc1155_metadata_collection.insert_erc1155_metadata(erc1155_metadata).await?;
+        }
+    } else {
         // We know that from balance won't be zero
-        let from_balance =
-            match erc1155_collection.get_erc1155_balance(contract_address, token_id, sender).await?
-            {
-                Some(v) => v,
-                None => {
-                    println!("Impossible state, from balance 0, using amount as default");
-                    amount
-                }
-            };
+        let from_balance = if let Some(balance) =
+            erc1155_collection.get_erc1155_balance(contract_address, token_id, sender).await?
+        {
+            balance
+        } else {
+            println!("Impossible state, from balance 0, using amount as default");
+            amount
+        };
 
         let new_balance = from_balance - amount;
         erc1155_collection
             .update_erc1155_balance(contract_address, token_id, sender, new_balance)
             .await?;
-    } else {
-        // Check if contract metadata exists
-        let metadata_exists =
-            contract_metadata_collection.contract_metadata_exists(contract_address).await?;
-
-        if !metadata_exists {
-            let name = contract::get_name(contract_address, &block_id, context.rpc).await;
-            let symbol = contract::get_symbol(contract_address, &block_id, context.rpc).await;
-            let contract_metadata = ContractMetadata::new(contract_address, name, symbol);
-            contract_metadata_collection.insert_contract_metadata(contract_metadata).await?;
-        }
     }
 
     // Update to balance
@@ -98,7 +120,7 @@ pub async fn handle_transfer(
                 .insert_erc1155_balance(Erc1155Balance::new(
                     contract_address,
                     token_id,
-                    recipient,
+                    sender,
                     amount,
                 ))
                 .await?;
