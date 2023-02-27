71edd12b25c7a9200ad69307b9c69c8cef6b60af
zetsuboii <hmzkrb@gmail.com>
2023-02-27 20:22:05 +0300

    feat(sqlx): add token_metadata and make blocks bigint

--- src/db/postgres/migrations/V1__create_metadata_table.sql
+++ src/db/postgres/migrations/V1__create_metadata_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE contract_metadata(
   contract_type t_contract_type NOT NULL,
   name TEXT,
   symbol TEXT,
-  last_updated INTEGER
+  last_updated_block BIGINT DEFAULT 0
 );
 
 CREATE TABLE token_metadata(
@@ -15,5 +15,14 @@ CREATE TABLE token_metadata(
   contract_type t_contract_type NOT NULL,
   token_id_low VARCHAR(80) NOT NULL,
   token_id_high VARCHAR(80) NOT NULL,
-  metadata TEXT NOT NULL
+  -- Metadata
+  image TEXT,
+  image_data TEXT,
+  external_url TEXT,
+  description TEXT,
+  name TEXT,
+  attributes TEXT[],
+  background_color TEXT,
+  animation_url TEXT,
+  youtube_url TEXT
 );
--- src/db/postgres/migrations/V3__create_erc1155_table.sql
+++ src/db/postgres/migrations/V3__create_erc1155_table.sql
@@ -1,9 +1,10 @@
-CREATE TABLE erc1155_balance(
+CREATE TABLE erc1155_balances(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   contract_address VARCHAR(80) NOT NULL,
   token_id_low VARCHAR(80) NOT NULL,
   token_id_high VARCHAR(80) NOT NULL,
+  account VARCHAR(80) NOT NULL,
   balance_low VARCHAR(80) NOT NULL,
   balance_high VARCHAR(80) NOT NULL,
-  last_update_block BIGINT DEFAULT 0
+  last_updated_block BIGINT DEFAULT 0
 );
