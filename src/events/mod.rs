6783d9b916c1bdfffadc3089f41bbd3602d51710
zetsuboii <hmzkrb@gmail.com>
2023-06-16 03:08:54 +0300

    feat: update main to use diffs

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,8 +1,10 @@
 pub mod erc1155;
 pub mod erc721;
 
+use async_trait::async_trait;
 use color_eyre::eyre::Result;
 use serde::{Deserialize, Serialize};
+use sqlx::{Pool, Postgres};
 use starknet::{
     core::types::FieldElement,
     providers::jsonrpc::{
@@ -25,17 +27,131 @@ use self::{
     erc721::transfer::Erc721Transfer,
 };
 
-pub struct EventHandler<'a, 'b> {
-    rpc: &'a JsonRpcClient<HttpTransport>,
-    transaction: &'b mut sqlx::Transaction<'a, sqlx::Postgres>,
+#[async_trait]
+pub trait IntoEventDiff {
+    async fn into_event_diff(self) -> EventDiff;
 }
 
-impl<'a, 'b> EventHandler<'a, 'b> {
-    pub fn new(
-        rpc: &'a JsonRpcClient<HttpTransport>,
-        transaction: &'b mut sqlx::Transaction<'a, sqlx::Postgres>,
-    ) -> Self {
-        EventHandler { rpc, transaction }
+pub struct Erc721Diff {
+    contract_address: FieldElement,
+    token_id: FieldElement,
+    new_owner: String,
+    timestamp: u64,
+}
+
+pub struct Erc1155Diff {
+    contract_address: FieldElement,
+    token_id: FieldElement,
+    // ( positive, amount )
+    balance_change: (bool, FieldElement),
+}
+
+pub enum EventDiff {
+    Erc721(Erc721Diff),
+    Erc1155(Erc1155Diff),
+}
+
+pub struct BlockDiff {
+    batch_id: u64,
+    events: Vec<EventDiff>,
+}
+
+impl BlockDiff {
+    pub fn new(batch_id: u64, events: Vec<EventDiff>) -> Self {
+        Self { batch_id, events }
+    }
+
+    pub fn batch_id(&self) -> u64 {
+        self.batch_id
+    }
+
+    pub fn events(&self) -> &[EventDiff] {
+        self.events.as_ref()
+    }
+}
+
+pub struct EventHandler<'a> {
+    rpc: &'a JsonRpcClient<HttpTransport>,
+    pool: &'a Pool<Postgres>,
+}
+
+impl<'a> EventHandler<'a> {
+    pub fn new(rpc: &JsonRpcClient<HttpTransport>, pool: &Pool<Postgres>) -> Self {
+        EventHandler { rpc, pool }
+    }
+
+    pub async fn diff_from_events(
+        &mut self,
+        batch_id: u64,
+        events: &[EmittedEvent],
+    ) -> Result<BlockDiff> {
+        let mut event_diffs = Vec::<EventDiff>::new();
+
+        for event in events {
+            match self.diff_from_event(event).await {
+                Ok(diff) => event_diffs.push(diff),
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+
+        Ok(BlockDiff::new(batch_id, event_diffs))
+    }
+
+    pub async fn diff_from_event(&mut self, event: &EmittedEvent) -> Result<EventDiff> {
+        let block_id = BlockId::Number(event.block_number);
+        let contract_address = event.from_address;
+
+        let blacklisted = sqlx::query!(
+            r#"
+                SELECT EXISTS (
+                    SELECT 1
+                    FROM blacklisted_contracts
+                    WHERE address = $1
+                )
+            "#,
+            contract_address.to_string()
+        )
+        .fetch_one(&self.pool)
+        .await?
+        .exists
+        .unwrap_or_default();
+
+        if blacklisted {
+            color_eyre::eyre::bail!("Contract blacklisted")
+        }
+
+        let keys = &event.keys;
+        match keys {
+            [&TRANSFER_EVENT_KEY, ..] => {
+                // Both ERC20 and ERC721 contracts use same event key to represent transfers so
+                // we have to check if the contract is ERC721 and blacklist if not so.
+                let is_erc721 = contract::is_erc721(contract_address, &block_id, self.rpc).await?;
+
+                if is_erc721 {
+                    let erc721_transfer = Erc721Transfer::from(event);
+                    erc721_transfer.process(self).await?;
+                } else {
+                    // Blacklist the non-ERC721 token
+                    sqlx::query!(
+                        r#"
+                        INSERT INTO blacklisted_contracts(address)
+                        VALUES ($1)
+                        ON CONFLICT DO NOTHING
+                    "#,
+                        contract_address.to_string()
+                    )
+                    .execute(&self.pool)
+                    .await?;
+                }
+            }
+            [&TRANSFER_SINGLE_EVENT_KEY, ..] => {
+                todo!()
+            }
+            [&TRANSFER_BATCH_EVENT_KEY, ..] => {
+                todo!()
+            }
+            _ => {}
+        }
     }
 
     pub async fn handle(&mut self, event: &EmittedEvent) -> Result<()> {
--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,83 @@ mod db;
 mod events;
 mod file_storage;
 mod rpc;
+use sqlx::{Pool, Postgres};
+use std::collections::BinaryHeap;
+use tokio::sync::mpsc::{self, Receiver};
 
 use color_eyre::eyre::Result;
 use dotenv::dotenv;
+
+use crate::{events::BlockDiff, rpc::StarknetRpc};
+
+const EVENT_CHANNEL_BUFFER_SIZE: usize = 10;
+const DEFAULT_STARTING_BLOCK: u64 = 1630; // First ERC721 transfer event
+const BLOCK_RANGE: u64 = 10;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
 
-    let rpc = rpc::connect()?;
     let pool = db::postgres::connect().await?;
 
-    // Drop everything from tables
+    // Drop everything from tables ('course for testing)
     db::postgres::drop_everything(&pool).await?;
 
-    //first transfer event is in 1630
-    let mut start_block = db::postgres::last_synced_block(&pool).await.unwrap_or(1630);
-    let range = 10;
+    let start_block =
+        db::postgres::last_synced_block(&pool).await.unwrap_or(DEFAULT_STARTING_BLOCK);
 
-    while start_block < 16000 {
-        println!("getting events between block {} and {}", start_block, start_block + range);
-        let transfer_events = rpc::get_transfer_events::run(start_block, range, &rpc).await?;
-        println!("got {} events in total", transfer_events.len());
+    // Both RPC and pool are needed to be instantiated once and used read-only. That's why we're
+    // leaking it and get static references out of them
+    let rpc: &'static StarknetRpc = Box::leak(Box::new(StarknetRpc::mainnet().unwrap()));
+    let pool: &'static Pool<Postgres> = Box::leak(Box::new(pool));
 
-        let mut transaction = pool.begin().await?;
-        let mut event_handler = events::EventHandler::new(&rpc, &mut transaction);
+    let (event_tx, event_rx) = mpsc::channel::<BlockDiff>(EVENT_CHANNEL_BUFFER_SIZE);
 
-        for transfer_event in &transfer_events {
-            event_handler.handle(transfer_event).await?;
+    // Spawn the task that'll read each event_diff message and save them to the database
+    tokio::spawn(async move { process_event_diffs(event_rx, &pool.clone()).await });
+
+    for batch_id in 0_u64.. {
+        tokio::spawn(async move {
+            let handler = events::EventHandler::new(rpc.clone().inner(), &pool.clone());
+
+            if let Ok(transfer_events) = rpc.get_transfer_events(start_block, BLOCK_RANGE).await {
+                let event_tx = event_tx.clone();
+                let block_diff = handler.diff_from_events(batch_id, &transfer_events).await?;
+                event_tx.send(block_diff).await;
+            }
+        });
+    }
+
+    Ok(())
+}
+
+async fn process_event_diffs(
+    mut event_rx: Receiver<BlockDiff>,
+    pool: &Pool<Postgres>,
+) -> Result<()> {
+    let mut latest_batch_id: u64 = 0;
+    let mut block_num_heap = BinaryHeap::<u64>::with_capacity(EVENT_CHANNEL_BUFFER_SIZE);
+    let mut blocks = Vec::<BlockDiff>::with_capacity(EVENT_CHANNEL_BUFFER_SIZE);
+
+    for block in event_rx.recv().await {
+        block_num_heap.push(block.batch_id());
+        blocks.push(block);
+
+        while block_num_heap.peek() == Some(latest_batch_id + 1).as_ref() {
+            // Process and pop pending block
+            let pending_block_idx = blocks
+                .iter()
+                .position(|item| item.batch_id() == block_num_heap.pop().unwrap())
+                .unwrap();
+            let pending_block = blocks.remove(pending_block_idx);
+
+            let transaction = pool.begin().await?;
+
+            // TODO: Proces the block diff
+
+            latest_batch_id += 1;
+            transaction.commit().await?;
         }
-        db::postgres::update_last_synced_block(start_block, &mut transaction).await?;
-        transaction.commit().await?;
-
-        println!("events handled");
-
-        start_block += range;
     }
 
     Ok(())
